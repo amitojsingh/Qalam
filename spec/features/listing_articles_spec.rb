20947d0b0f16114b2573207300b5e24e819dc135
Amitoj Singh <amitojsingh95@gmail.com>
2017-01-16 16:00:15 +0530

    listening articles checked

---
+++ spec/features/listing_articles_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.feature "Listing Articles" do 
+
+	before do 
+		@article1=Article.create(title: "title1", body: "body of first article")
+		@article2=Article.create(title: "title2", body: "body of secon article")
+
+	end
+
+	scenario "A user list all articles" do 
+		
+		visit "/"
+		
+		expect(page).to have_content(@article1.title)
+		expect(page).to have_content(@article1.body)
+		expect(page).to have_content(@article2.title)
+		expect(page).to have_content(@article2.body)
+		expect(page).to have_link(@article1.title)
+		expect(page).to have_link(@article2.title)
+	end
+
+	scenario "A user has no articles" do 
+
+		Article.delete_all
+
+		visit "/" 
+		
+		expect(page).not_to have_content(@article1.title)
+		expect(page).not_to have_content(@article2.title)
+		expect(page).not_to have_content(@article1.body)
+		expect(page).not_to have_content(@article2.body)
+		expect(page).not_to have_link(@article1.title)
+		expect(page).not_to have_link(@article2.title)
+		within("h1#no-articles") do 
+			expect(page).to have_content("no articles created")
+		end
+	end
+
+end
+
