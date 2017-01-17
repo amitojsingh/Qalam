b6818225fc68aa9179d589230decc7f9469f678b
Amitoj Singh <amitojsingh95@gmail.com>
2017-01-17 23:54:05 +0530

    signup updated

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Signup users" do 
+	scenario "with valid credentials" do 
+		visit "/"
+		click_link "Sign up"
+		fill_in "Email", with: "Amitojsingh95@gmail.com"
+		fill_in "Password", with: "123456"
+		fill_in "Password confirmation",with: "123456"
+		click_button "Sign up"
+
+	#	expect(page).to have_content("You have signed up sucessfully")
+
+	end
+
+	scenario "with invalid credentials" do 
+		visit "/"
+		click_link "Sign up"
+		fill_in "Email", with: ""
+		fill_in "Password", with: ""
+		fill_in "Password confirmation",with: ""
+		click_button "Sign up"
+
+	#	expect(page).to have_content("You have not signed up sucessfully")
+	end
+end
