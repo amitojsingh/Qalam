43872f4605a0cac258611928488b4d9320a53c46
Amitoj Singh <amitojsingh95@gmail.com>
2017-01-19 13:18:17 +0530

    fix edit-show

--- spec/features/show_articles_spec.rb
+++ spec/features/show_articles_spec.rb
@@ -4,18 +4,34 @@ RSpec.feature "Showing an article" do
 
 	before do
 		@john=User.create(email: "john@gmail.com",password: "password")
-		login_as(@john)
+		@faiz=User.create(email: "faiz@gamil.com",password:"123456")
 		@article=Article.create(title: "Title of the article", body: "BOdy of the article",user: @john)
 	end
 
-	scenario "A user shows an article" do
+	scenario "not signed in user hide edit and delete button" do
 		visit "/"
 
 		click_link @article.title
 
 		expect(page).to have_content(@article.title)
 		expect(page).to have_content(@article.body)
+		expect(page).not_to have_link("Edit Article")
+		expect(page).not_to have_link("Delete Article")
 		expect(current_path).to eq(article_path(@article))
+end
 
+scenario "to not-owner signed in user show edit and delete button" do
+
+ login_as(@faiz)
+	visit "/"
+
+
+	click_link @article.title
+
+	expect(page).to have_content(@article.title)
+	expect(page).to have_content(@article.body)
+	expect(page).not_to have_link("Edit")
+	expect(page).not_to have_link("delete")
+	expect(current_path).to eq(article_path(@article))
 end
 end
