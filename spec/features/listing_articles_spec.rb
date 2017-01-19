aa45ebf0d457199fa4218d24c3abef1fb753177d
Amitoj Singh <amitojsingh95@gmail.com>
2017-01-19 13:01:30 +0530

    fix new article

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -6,10 +6,11 @@ RSpec.feature "Listing Articles" do
 		@john=User.create(email: "john@example.com",password: "password")
 		@article1=Article.create(title: "title1", body: "body of first article", user: @john)
 		@article2=Article.create(title: "title2", body: "body of secon article", user: @john)
+		login_as(@john)
 
 	end
 
-	scenario "A user list all articles" do
+	scenario "With articles created and user signed in" do
 
 		visit "/"
 
@@ -21,6 +22,22 @@ RSpec.feature "Listing Articles" do
 		expect(page).to have_content("Created by #{@article2.user.email}")
 		expect(page).to have_link(@article1.title)
 		expect(page).to have_link(@article2.title)
+		expect(page).to have_link("New Article")
+	end
+
+	scenario "with articles created and user not signed in" do
+
+		visit "/"
+
+		expect(page).to have_content(@article1.title)
+		expect(page).to have_content(@article1.body)
+		expect(page).to have_content("Created by #{@article1.user.email}")
+		expect(page).to have_content(@article2.title)
+		expect(page).to have_content(@article2.body)
+		expect(page).to have_content("Created by #{@article2.user.email}")
+		expect(page).to have_link(@article1.title)
+		expect(page).to have_link(@article2.title)
+		expect(page).to have_link("New Article")
 	end
 
 	scenario "A user has no articles" do
