2d39adf43020493a0529cd9e03a170b7c9d2a552
amitoj singh <amitojsingh95@gmail.com>
2018-01-28 23:36:59 +0530

    final drift

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,5 @@
+ $(document).on('turbolinks:load', function() {
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
+});
 });
