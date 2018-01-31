02036aee39a605d64bc3a5539a577346708d74b4
amitoj singh <amitojsingh95@gmail.com>
2018-01-31 23:13:25 +0530

    design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { host: 'ancient-caverns-61942.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://ancient-caverns-61942.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
