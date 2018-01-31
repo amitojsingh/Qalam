46ead1735bedd2b03e3a62ef518a2f691603e5fb
amitoj singh <amitojsingh95@gmail.com>
2018-01-31 15:23:20 +0530

    improved

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,20 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: 'ancient-caverns-61942.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name:"amitojsingh95@gmail.com",
+  password: "signingmail1004"
+  }
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
